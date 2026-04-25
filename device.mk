3f5ad19ecdcafd4c6a6db18b140d7ffee8de21dc
梅梅不是没没 <163523732+mmbsmm-git@users.noreply.github.com>
2026-04-25 21:50:09 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,8 +1,11 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/youxuepai/k71v1_64_bsp
+
+# 针对 TWRP
+$(call inherit-product, build/target/product/embedded.mk)
+
+# 基本产品信息
+PRODUCT_DEVICE := k71v1_64_bsp
+PRODUCT_NAME := omni_k71v1_64_bsp
+PRODUCT_BRAND := youxuepai
+PRODUCT_MODEL := k71v1_64_bsp
+PRODUCT_MANUFACTURER := youxuepai
